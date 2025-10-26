3455123c7b3d376b8c5a84b641bc6bd3cb2cf853
alexander vagin <sashavagin1987@gmail.com>
2025-10-26 22:55:44 +1000

    - изменен адрес для UserHabitsListAPIView с '', 'user/'

--- habits/urls.py
+++ habits/urls.py
@@ -7,10 +7,10 @@ from habits.views import UserHabitsListAPIView, PublicHabitsListAPIView, HabitsC
 app_name = "habits"
 
 urlpatterns = [
+    path('user/', PublicHabitsListAPIView.as_view(), name='habits_list'),
     path('public/', UserHabitsListAPIView.as_view(), name='habits_public_list'),
-    path('', PublicHabitsListAPIView.as_view(), name='habits_list'),
     path('create/', HabitsCreateAPIView.as_view(), name='habits_create'),
-    path('update/', HabitsUpdateAPIView.as_view(), name='habits_update'),
-    path('detail/', HabitsRetrieveAPIView.as_view(), name='habits_detail'),
-    path('delete/', HabitsDestroyAPIView.as_view(), name='habits_delete'),
+    path('update/<int:pk>/', HabitsUpdateAPIView.as_view(), name='habits_update'),
+    path('detail/<int:pk>/', HabitsRetrieveAPIView.as_view(), name='habits_detail'),
+    path('delete/<int:pk>/', HabitsDestroyAPIView.as_view(), name='habits_delete'),
 ]

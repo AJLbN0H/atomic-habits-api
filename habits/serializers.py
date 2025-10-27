482f6d3a44be8a47dd616a0abe363cd646600ad1
alexander vagin <sashavagin1987@gmail.com>
2025-10-27 23:18:05 +1000

    - реализована валидация полей: associated_habit, reward, time_to_complete

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from habits.models import Habit
@@ -9,3 +10,20 @@ class HabitsSerializer(ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
+
+    def validate(self, data):
+        """Валидация проверяющая:
+         - выбрал пользователь вознаграждение или приятную привычку, но не оба варианта.
+         - что время выполнения должно быть не больше 120 секунд"""
+
+        associated_habit = data.get('associated_habit')
+        reward = data.get('reward')
+        time_to_complete = data.get('time_to_complete')
+
+        if associated_habit and reward:
+            raise serializers.ValidationError("Нельзя выбрать одновременно вознаграждение и приятную привычку")
+
+        if time_to_complete > 120:
+            raise serializers.ValidationError("Время выполнения должно быть не больше 120 секунд")
+
+        return data

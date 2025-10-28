4c89a54cbd8a52e38e2d977d3c77a84761dfcce6
alexander vagin <sashavagin1987@gmail.com>
2025-10-28 18:14:52 +1000

    - в settings.py добавлена настройка вызова API_TELEGRAM_BOT_FATHER

--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+API_TELEGRAM_BOT_FATHER = os.getenv('API_TELEGRAM_BOT_FATHER')
